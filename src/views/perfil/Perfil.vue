31a8ce4775801a8430e97837c2e3d3f11831a41d
MaceiraDev <leo.maceira.santos@hotmail.com>
2024-10-10 21:22:25 -0300

    ajuste em perfil

--- src/views/perfil/Perfil.vue
+++ src/views/perfil/Perfil.vue
@@ -4,9 +4,9 @@
       <hr class="bg-azul2 h-0.5 mt-2 mb-4" />
 
       <div class="flex items-center mb-4">
-         <img :src="user_image" alt="Imagem do Usuário" class="w-20 h-20 rounded-full object-cover mr-4" />
+         <img :src="state.imagem" alt="Imagem do Usuário" class="w-20 h-20 rounded-full object-cover mr-4" />
          <div>
-            <h2 class="text-xl font-bold">{{ user_nome }}</h2>
+            <h2 class="text-xl font-bold">{{ state.nome }}</h2>
             <p class="text-gray-600">{{ tipoUsuario }}</p>
             <p class="text-gray-600">{{ state.whatsapp }}</p>
             <p class="text-gray-600">{{ state.status }}</p>
@@ -27,17 +27,13 @@ import services from '@/services';
 
 const storage = useStorage();
 const token = storage.getStorageSync("token");
-const user_nome = storage.getStorageSync("nome");
-const user_image = storage.getStorageSync("imagem");
 const user_id = storage.getStorageSync("user_id");
 const user_tipo = storage.getStorageSync("tipo_usuario");
 
 const state = reactive({
    whatsapp: '',
-   status: 'ATIVO',
+   status: '',
    notas_adicionais: '',
-   loader: false,
-   modal: false,
    MensagemErro: "",
 });
 
@@ -70,15 +66,17 @@ function formatarWhatsApp(whatsapp) {
       return `(${whatsapp.slice(0, 2)}) ${whatsapp[2]} ${whatsapp.slice(3, 7)}-${whatsapp.slice(7)}`;
    } else {
       console.error('Número de WhatsApp inválido:', whatsapp);
-      return whatsapp; 
+      return whatsapp;
    }
 }
 
 async function buscarUsuarioId(id) {
    try {
       const { response } = await services.usuarios.getById(id, token);
+      state.nome = response.nome;
       state.whatsapp = formatarWhatsApp(response.whatsapp);
       state.status = response.status;
+      state.imagem = response.imagem;
       state.notas_adicionais = response.notas_adicionais;
    } catch (error) {
       console.error('Erro ao buscar usuário:', error);
