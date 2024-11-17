dece5bd3440e9754964e704c234bfb6e95f21ef2
MaceiraDev <leo.maceira.santos@hotmail.com>
2024-11-17 12:27:49 -0300

    ajuste de dashboard

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -1,6 +1,6 @@
 <template>
    <section id="dashboard" class="p-3">
-      <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4 mb-6">
+      <div class="grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-4 gap-4 mb-6">
          <div class="bg-azul2 rounded-lg p-4 flex flex-col items-center card">
             <h2 class="text-lg font-semibold text-branco">Fichas Pendentes</h2>
             <p class="text-3xl font-bold text-limao">{{ state.data_card.fichas_pendentes }}</p>
@@ -18,7 +18,8 @@
             <p class="text-3xl font-bold text-limao">{{ state.data_card.pets_totais }}</p>
          </div>
       </div>
-      <div class="grid grid-cols-1 lg:grid-cols-2 gap-4 mt-12">
+      <div class="grid grid-cols-1 lg:grid-cols-1 xl:grid-cols-1 2xl:grid-cols-2 gap-4 mt-12">
+         <!-- Fichas Pendentes Table -->
          <div class="overflow-x-auto">
             <h3 class="text-lg text-preto font-medium">Fichas Pendentes/Recentes</h3>
             <table class="min-w-full table-auto bg-table2 shadow-lg overflow-hidden divide-y-4 divide-limao">
@@ -43,7 +44,7 @@
                      <td class="px-4 py-2">
                         <router-link :to="'/fichas/alterar-ficha/' + ficha.id">
                            <button
-                              class="bg-azul1 text-branco px-4 py-2 rounded-md transition duration-500 hover:bg-azul4 hover:text-preto ">
+                              class="bg-azul1 text-branco px-4 py-2 rounded-md transition duration-500 hover:bg-azul4 hover:text-preto">
                               Ir para a ficha
                               <i class="bi bi-person-walking"></i>
                            </button>
@@ -53,14 +54,8 @@
                </tbody>
             </table>
          </div>
-         <div class="grid grid-cols-1 lg:grid-cols-2 gap-4">
-            <VueApexCharts type="pie" width="380" :options="chartOptions" :series="series" />
-            <div class="chart-wrapper">
-               <VueApexCharts type="pie" :options="chartOptionsTwo" :series="seriesTwo" />
-            </div>
-         </div>
-      </div>
-      <div class="grid grid-cols-1 lg:grid-cols-2 gap-4 mt-12">
+
+         <!-- Banhos e Tosas Agendados Table -->
          <div class="overflow-x-auto">
             <h3 class="text-lg text-preto font-medium">Banhos e Tosas Agendados</h3>
             <table class="min-w-full table-auto bg-table2 shadow-lg overflow-hidden divide-y-4 divide-limao">
@@ -85,7 +80,8 @@
                            <button
                               class="bg-azul1 text-branco px-4 py-2 rounded-md transition duration-500 hover:bg-azul4 hover:text-preto ">
                               Visualizar Banho
-                              <i class="bi bi-person-walking"></i></button>
+                              <i class="bi bi-droplet"></i>
+                           </button>
                         </router-link>
                      </td>
                   </tr>
@@ -93,12 +89,30 @@
             </table>
          </div>
       </div>
+      <div class="grid grid-cols-1 lg:grid-cols-1 xl:grid-cols-2 2xl:grid-cols-2 gap-4 mt-12">
+         <div class="text-center">
+            <h3 class="text-lg text-preto font-medium">Serviços por Mês</h3>
+            <VueApexCharts type="area" :options="chartOptionsServices" :series="seriesServices" width="100%" />
+         </div>
+         <div
+            class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-1 xl:grid-cols-1 2xl:grid-cols-2 gap-4">
+            <div class="text-center">
+               <h3 class="text-lg text-preto font-medium">Status Fichas</h3>
+               <VueApexCharts type="pie" width="380" :options="chartOptions" :series="series" />
+            </div>
+            <div class="chart-wrapper text-center">
+               <h3 class="text-lg text-preto font-medium">Pets por Espécies</h3>
+               <VueApexCharts type="pie" :options="chartOptionsTwo" :series="seriesTwo" />
+            </div>
+         </div>
+
+      </div>
    </section>
 </template>
 
 <script setup>
 import services from "@/services";
-import { reactive, ref, onMounted, computed } from "vue";
+import { reactive, onMounted, computed } from "vue";
 import VueApexCharts from "vue3-apexcharts";
 import { useStorage } from 'vue3-storage';
 
@@ -113,6 +127,10 @@ const state = reactive({
       fichas: [],
       banhos: [],
    },
+   data_servicos: {
+      clinico: {},
+      estetico: {},
+   }
 });
 
 onMounted(() => {
@@ -121,11 +139,11 @@ onMounted(() => {
 
 async function getInfos() {
    const { response } = await services.info.getInfos(token);
-   state.data = response;
    state.data_status = response.graficos.total_status_fichas;
    state.data_card = response.cards;
    state.data_tabelas = response.tabelas;
    state.pets_por_especie = response.graficos.pets_por_especie;
+   state.data_servicos = response.graficos.totais_servicos;
 }
 
 const chartOptions = reactive({
@@ -135,10 +153,10 @@ const chartOptions = reactive({
    labels: ['Fichas pendentes', 'Fichas em andamento', 'Fichas concluídas', 'Fichas canceladas'],
    responsive: [
       {
-         breakpoint: 480,
+         breakpoint: 400,
          options: {
             chart: {
-               width: 200,
+               width: 200, // Reduz o tamanho do gráfico para telas menores
             },
             legend: {
                position: "bottom",
@@ -179,8 +197,66 @@ const chartOptionsTwo = computed(() => ({
 
 const seriesTwo = computed(() => state.pets_por_especie.map(item => item.total));
 
+const chartOptionsServices = reactive({
+   chart: {
+      type: "area",
+      height: 350,
+   },
+   dataLabels: {
+      enabled: false,
+   },
+   stroke: {
+      curve: "smooth",
+   },
+   colors: ['#02416d', '#8acd1b'],
+   xaxis: {
+      categories: [], // Meses serão populados dinamicamente
+      title: {
+         text: "Mês"
+      }
+   },
+   yaxis: {
+      title: {
+         text: "Total de Serviços"
+      }
+   },
+   legend: {
+      position: "top",
+      horizontalAlign: "center"
+   }
+});
 
+// Computed para configurar os dados dos serviços
+const seriesServices = computed(() => {
+   const clinico = state.data_servicos.clinico || {};  // Ajustado para clinico diretamente
+   const estetico = state.data_servicos.estetico || {}; // Ajustado para estetico diretamente
 
+   // Converte os objetos em arrays para acessar mês e total
+   const clinicoData = Object.values(clinico).map(item => ({
+      x: item.mes,
+      y: item.total
+   }));
+
+   const esteticoData = Object.values(estetico).map(item => ({
+      x: item.mes,
+      y: item.total
+   }));
+
+   // Popula as categorias no eixo X com os meses
+   const allMonths = [
+      ...new Set([
+         ...clinicoData.map(item => item.x),
+         ...esteticoData.map(item => item.x)
+      ])
+   ];
+
+   chartOptionsServices.xaxis.categories = allMonths;
+
+   return [
+      { name: 'Clínico', data: clinicoData },
+      { name: 'Estético', data: esteticoData }
+   ];
+});
 </script>
 
 <style scoped>
